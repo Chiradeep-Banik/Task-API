1ed28cb21b5b02d0de599f59cbb3963dd302de66
Chiradeep-Banik <chiradeep13@gmail.com>
2021-09-18 00:09:08 +0530

    Started using mongoose

--- routes/app.js
+++ routes/app.js
@@ -1,36 +1,63 @@
-console.log('app.js loaded');
-
-const {MongoClient: mongo_client, ObjectId } = require('mongodb');
+const express = require('express');
+const { mongoClient } = require('mongodb');
+const { Schema, model, connect, save, Collection } = require('mongoose');
+const { isEmail } = require('validator');
 
 const pw = 'QLjY6d6xdGDW49N';
 const uri = `mongodb+srv://banik_1313:${pw}@cluster0.gdrur.mongodb.net/Task-Manager?retryWrites=true&w=majority`;
 
-// mongo_client.connect(uri, { useNewUrlParser: true }, (err, client) => {
-//     if(err) return console.log(err);
-//     console.log('Connected to database');
+connect(uri);
 
-// });
-const client = mongo_client.connect(uri,{ useNewUrlParser:true });
+console.log('Connected to MongoDB');
+var task_schema = new Schema({
+    task_description : {
+        type: String,
+        required: true,
+        maxlength: 500
+    },
+    completed : {
+        type: Boolean,
+        default: false,
+        required: true
+    }
+});
 
-client.then(client => {
-    const db = client.db ('Task-Manager');
-    var users = db.collection('users');
-    var tasks = db.collection('tasks');
-
-    var p = users.deleteMany({name:'aaa'});
-    var p1 = tasks.insertMany([
-        {
-            _id: new ObjectId(),
-            name:'task1',
-            description:'task1 description',
-        },
-        {
-            _id: new ObjectId(),
-            name:'task2',
-            description:'task2 description',
+var user_schema = new Schema({
+    username : {
+        type : String,
+        required : true,
+        maxlength : 10
+    },
+    email : {
+        type : String,
+        required : true,
+        validate(val){
+            if(!isEmail(val))
+                throw new Error('Invalid email');
         }
-    ]);
-    p.then(result => console.log(result)).catch(err => console.log(err));
-    p1.then(result => console.log(result)).catch(err => console.log(err));
+    },
+    age : {
+        type: Number,
+        required: true,
+        maxlength: 10
+    }
+});
+var task = model('task',task_schema,'tasks');
+var user = model('user', user_schema,'users');
 
-}).catch(err => console.log(err));
+var t1 = new task({
+    task_description : 'Task 1 description',
+    completed : false
+});
+var user1 = new user({
+    username : 'user1',
+    email : 'qwe@123',
+    age : 20
+});
+
+user1.save().then(res=>console.log(res)).catch(err=>console.log(err));
+t1.save().then((res) => console.log(res));
+
+task.find({}).then((res) => console.log(res));
+
+
