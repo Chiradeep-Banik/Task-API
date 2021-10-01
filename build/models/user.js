0e9cb77c4d5f61d428961146b4d49f89f214372a
Chiradeep-Banik <chiradeep13@gmail.com>
2021-10-02 01:16:46 +0530

    Forgot to build

--- build/models/user.js
+++ build/models/user.js
@@ -1,11 +1,7 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.user = void 0;
 const mongoose_1 = require("mongoose");
-const isEmail_1 = __importDefault(require("validator/es/lib/isEmail"));
 ;
 ;
 const user_schema = new mongoose_1.Schema({
@@ -20,12 +16,12 @@ const user_schema = new mongoose_1.Schema({
         trim: true,
         toLowerCase: true,
         unique: true,
-        validate: {
-            validator(value) {
-                if (!(0, isEmail_1.default)(value))
-                    throw new Error("Invalid email");
-            }
-        }
+        // validate: {
+        //     validator(value:string){
+        //         if(!isEmail(value))
+        //             throw new Error("Invalid email");
+        //     }
+        // }
     },
     password: {
         type: String,
